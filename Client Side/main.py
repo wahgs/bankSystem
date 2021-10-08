01959526810b1ef35473580961c9bfeacd14a6dd
grizhe <69319142+grizhe@users.noreply.github.com>
2021-10-08 10:57:00 -0400

    Organized Folders, created Ideas.txt, worked on main login stack, updated To-do list in Repository's project. (work done in school :} )

--- Client Side/main.py
+++ Client Side/main.py
@@ -8,16 +8,22 @@ passkey = False
 seckey = False
 acnum = False
 loginsequence = True
-#Begin LoginSequence
-while loginsequence == True:
+userstack = True
+#Begin LoginStack
+while loginstack == True:
     # Usernames --
-    while True:
+    while userstack == True:
         if userkey:
             continue
         elif not userkey:
             inp1 = input("Welcome, please type your username.")
-            f.usernameFunction(str(inp1))
+            #Checks to see if the username is a username in database
+            try:
+                f.usernameFunction(str(inp1))
+            except:
+                print("Exception caught in Username Stack")
             print("Welcome, " + str(accounts.ky.get('name')).title() + ". Please enter your password:")
+            userstack = False
             continue
         else:
             f.disband()
@@ -30,7 +36,9 @@ while loginsequence == True:
             f.passFunction(str(inp2))
             f.accountNumberVerifier(accounts.ky.get('accountnum'))
             print("Thank you, " + str(accounts.ky.get('name')).title() + ".")
-            loginsqeuence = False
+            loginstack = False
             continue
         else:
             f.disband()
+    #end userstack
+    
