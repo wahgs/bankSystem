ebdc1cc21ddf5ea0d72c8c2513d614e0cd321c84
grizhe <69319142+grizhe@users.noreply.github.com>
2021-10-27 12:26:18 -0400

    Started development and mental ideas for server side: learning how to use socket for this program... :)

---
+++ Server Side/real code/serverf.py
@@ -0,0 +1,5 @@
+import hashlib
+import time
+import sys
+import servermain
+#functions "class'" for servermain.py developed by grizhe
---
+++ Server Side/real code/servermain.py
@@ -0,0 +1,13 @@
+import hashlib
+import time
+import sys
+import serverf
+#server side for bankSystem developed by grizhe
+#this is acting as the server side recieving information from the "client side"
+def clientinput(firstInput, secondInput):
+    inp1 = firstInput
+    inp2 = secondInput
+    sys.excepthook
+
+while True:
+    if inp1 == ''
