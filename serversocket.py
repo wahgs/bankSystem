d5b4790809267ba1efce23e57e78519b5f18df8d
grizhe <69319142+grizhe@users.noreply.github.com>
2021-11-01 11:00:44 -0400

    hi

--- serversocket.py
+++ serversocket.py
@@ -9,25 +9,29 @@ import socketsetupprototype
 #waits for client to connect, and then
 #establishes a fluid socket connection
 def socketsetup():
-    s=socket.socket()
-    host=socket.gethostname()
-    port=12000 
-    s.bind((host,port))
-    s.listen(10)
     while True:
-        c,addr=s.accept()
-        print("Client connected" + addr)
-        print('Got Connection from' + addr)
-        content=c.recv(100).decode()
-        if not content:
-            break
-        print()
+        s=socket.socket()
+        host=socket.gethostname()
+        port=12000 
+        s.bind((host,port))
+        s.listen(10)
+        if verify(c):
+            continue
+        elif not verify(c):
+            disband("Key could not be located.")
+        while True:
+            c,addr=s.accept()
+            print("Client connected" + addr)
+            print('Got Connection from' + addr)
+            content=c.recv(100).decode()
+            if not content:
+                break
+            print()
 
 def keyRequest(num):
     #make program request verification key from clientside
-    input("hi")
-
 
 def verify(verificationKey):
     #make program verify key that was sent from program
-    input("hi")
+    if accounts.ky.get('seckey') is str(verificationKey):
+        print("Security code, " + str(verificationKey) + "verified.")
