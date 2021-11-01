53579455a77edc459522b8ad33bbf6c62b1317d6
grizhe <69319142+grizhe@users.noreply.github.com>
2021-11-01 12:00:40 -0400

    i need to do my schoolwork

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ import f
 
 print("Connecting to server...")
 
-f.socketsetup('192.168.0.56', '613')
+#loop for attempting to connect to the server
+while True:
+    try: f.socketsetup('192.168.0.56', '613')
+        print("Connected to server: ")
+    except: Exception as e
