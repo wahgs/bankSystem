209da92cc5bc4ae6fca9c68c9a3cfcfe4e95d248
grizhe <grizhebusiness@gmail.com>
2022-08-14 19:58:07 -0400

    learning <:)

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
-from flask import Flask
+import re
+from flask import Flask, redirect, url_for, render_template, request
 import sys
 
 app = Flask(__name__)
 
+inp = ''
+
+def type():
+    global inp
+    inp = input("What would you like the website to say?")
+    return str(inp)
+
+@app.route("/")
+def home():
+    if request.method == "POST":
+        return redirect(url_for("login"))   
+    else:
+        return render_template("index.html", content='hi')
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form['nm']
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return render_template("loginpasswd.html", user=usr) 
+
+
+if __name__ == '__main__': 
+    app.run(debug=True)
