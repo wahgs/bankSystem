5a86457f33c3bb03985e1162c204029ea9b499a0
grizhe <grizhebusiness@gmail.com>
2022-09-18 11:09:56 -0400

    Created mariaConnector.py, small changes.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import mariadb
 import hashlib
 import random
+from time import sleep
 
 
 #default mariadb info----------------------------
@@ -11,19 +12,118 @@ password="a"
 host="localhost"
 port=3306
 database='accounts'
-try:
-    conn = mariadb.connect(
-        user=user,
-        password=password,
-        host=host,
-        port=port,
-        database=database
-    )
-    print("Maria successfully ")
-except Exception as e:
-    print("Exception: " + str(e) + "Probably caused by the server not being setup. Please run mariaFormatter.py to format your mariaDB server in docker.")
-    quit()
 
+#this is referenced in mariaconnector, saves space.
+def statement(var):
+    print(f"Please type new {var} that you would like to change below.")
+
+#this will be run before the website will go live
+#refer to mariaConector.txt for instructions, and explaination. As comments would not describe the complexity.
+def mariaConnector():
+    global conn, connected, user, password, host, port, database
+    #var turns false when the connection credentials are true
+    var = True
+    #statement is used 
+    print("Do you already have a database in your mariadb instance that you would like to link to this program?")
+    inp = input("[User: (y/n)] ")
+    if inp == 'y':
+        print('please define the proper name of the database that you would like to connect to in your provided mariadb instance.')
+        database = input("[User:] ")
+        print('Has the database that you defined above already been formatted by this program to be used with this program? [y/n] (if unsure type n)')
+        databasequery = input("[User:] ")
+        if databasequery == 'y':
+            databasequery = True
+        elif databasequery == 'n':
+            databasequery = False
+        while var:
+            try:
+                conn = mariadb.connect(
+                    user=username,
+                    password=password,
+                    host=host,
+                    port=port,
+                    database=database
+                )
+                connected = True
+                var = False
+                cur = conn.cursor()
+            except Exception as e:
+                print("MariaDB connection not default. Server error message below\n" + e)
+            print("Attmpted Credentials below.")
+            print(f"user='{user}', password='{password}', host={host}, port={str(port)}, database={database}")
+            happened = False
+            while not connected:
+                print("What credentials would you like to change? Type 'try again', or 'ta' to retry the connection.")
+                print('[username/password/host/pot/database/exit]')
+                inp = input('')
+                if inp.lower() == 'username':
+                    statement(inp.title())
+                    username = input('')
+                    continue
+                elif inp.lower() == 'password':
+                    statement(inp)
+                    password = input('')
+                    continue
+                elif inp.lower() == 'host':
+                    statement(inp)
+                    host = input('')
+                    continue
+                elif inp.lower() == 'port':
+                    statement(inp)
+                    port = input('')
+                    continue
+                elif inp.lower() == 'database':
+                    statement(inp)
+                    database = input('')
+                    continue
+                elif inp.lower() == 'exit' or 'try again' or 'ta':
+                    var = True
+        #this happens if the server is actually connected.
+        while not var:
+            print("Do you give this program permission to edit the database that you declared above? This means that the program will have permission modify and manipulate the defined database in your mariadb instance.")
+            inp = input("[y/n]: ")
+            if inp.lower() == 'y':
+                while True:
+                    #this needs to append all of the required tables into a database called bankSys using cur.execute... 
+                    cur.execute("")
+            elif inp.lower == 'n':
+                print('Okay, shutting down program, please start a mariadb instance that you would give modifying permissions to running before you start this program again.')
+                sleep(5)
+                sys.exit()
+    elif inp.lower() == 'n':
+        while True:
+            print("Okay, would you like to use a current database? Or create one? [use/create]")
+            DBQ = input("[User:] ")
+            cur.execute("SHOW DATABASES")
+            dblist = cur.fetchall()
+            if DBQ == 'use':
+                print("What is the name of the database that you would like to use? (CaSe sensitive)")
+                print("Here is a list of the databases that you have in your server:\n" + dblist)
+                dbname = input("\n[User:] ")
+                cur.execute("SHOW DATABASES LIKE '" + dbname + "';")
+                dbnamereturn = cur.fetchone()
+                if dbnamereturn == "None":
+                    print("This database does not exist, please try again.")
+                    continue
+                elif dbnamereturn != "None":
+                    print(f"Alright, we'll use the database, '{dbname}'!" )
+                    database = dbname
+                    break
+            elif DBQ == 'create':
+                print("What is the name that you would like for the database to be? {press enter for defualt")
+                dbname = input("[User:] ")
+                print("Attempting to create database")
+                if dbname == '':
+                    dbname = 'bankSys'
+                else:
+                    cur.execute("CREATE DATABASE " + dbname)
+                    cur.commit
+                cur.execute("SHOW DATABASES")
+                dblist = cur.fetchall()
+                if dbname in dblist:
+                    print("Success, the database was successfully created.")
+                elif dbname not in dblist:
+                    print("There was an error creating the database, please try again, or manually insert the database that you would like to use.")
 cur = conn.cursor()
 #end mariadb info---------------------------
 
@@ -87,68 +187,6 @@ def mariaContentChecker():
             "VALUES('username', 'password', 'secnum', 'bal');"
         )
 
-def datacheck():
-    global conn
-    #assigns information for the program to connect to the mariaDB docker instance
-    mariaConnectionInfoCorrect = False
-    try:
-        cur.execute("SHOW TABLES;")
-        print("")
-        mariaConnectionInfoCorrect = True
-    except Exception as e:
-        print("Connection invalid with default credentials. Please follow the prompt to set up your mariadb instance")
-        while mariaConnectionInfoCorrect == False:
-            print("What is the username?")
-            user = input('')
-            print("What is the password")
-            password = input('')
-            print("What is the host IP? (address can be 'localhost' as well.")
-            host = input('')
-            print("What is the port?")
-            port = int(input(''))
-            print('What is the database name? (press enter for default, default is accounts)')
-            database = input('')
-            if database == '':
-                database = 'accounts'
-            print("Okay, attempting to connect...")
-            try:
-                conn = mariadb.connect(
-                    user=user,
-                    password=password,
-                    host=host,
-                    port=port,
-                    database=database
-                )
-                print("Connected!")
-                if mariaContentChecker() == True:
-                    print('mariadb connection established.')
-                    return True
-
-            except Exception as e:
-                print(f"Error trying to connect: [{str(e)}]")
-                print(f"Please enter data again...")
-                mariaConnectionInfoCorrect = False
-                datacheck()
-    else:
-        try:
-            conn = mariadb.connect(
-                user="root",
-                password="a",
-                host="localhost",
-                port=3306,
-                database='accounts'
-            )
-            print("Connected!")
-            mariaContentChecker()
-        except Exception as e:
-                print(f"Error trying to connect: [{str(e)}]")
-                print(f"Please enter data again...")
-                datacheck()
-
-
-def mariacheck():
-    print()
-    datacheck()
 
 
 def startup():
