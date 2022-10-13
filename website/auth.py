f230e95ad4deeb1e2dc7dd3aa64016762eb3cd52
grizhe <grizhebusiness@gmail.com>
2022-10-13 18:27:04 -0400

    work

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,11 @@
 #holds authentications
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -13,4 +13,4 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign up</p>"
+    return render_template("sign_up.html")
