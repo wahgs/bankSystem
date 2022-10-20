b4c7fe81a81db0b632e3fb18addff740e0b8d536
grizhe <grizhebusiness@gmail.com>
2022-10-20 03:10:27 -0400

    it all works >:)

--- website/views.py
+++ website/views.py
@@ -1,13 +1,13 @@
 #stores url endpoints for home, removing notes,
-from unicodedata import category
 from flask import Blueprint, render_template, flash, request, redirect, url_for
 from flask_login import login_required, current_user
 from .models import User
+from .auth import genbal
 from . import db
 import json
 import jsonify
 import random
-
+from time import sleep
 
 
 views = Blueprint('views', __name__)
@@ -24,35 +24,47 @@ def duh():
 
 
 
-@views.route("/home", methods =['GET', 'POST'])
-@login_required
+@views.route("/home")
+@login_required 
 def home():
-    if request.method == 'POST':
-        #for returns
-        pass
-    
-    else:
-        #if the user has a balance set.
-        if current_user.balance:
-            return render_template("home.html", firstName=current_user.first_name, user=current_user, balance=current_user.balance)
-        #if the user does not have a balance
-        elif not current_user.balance:
-            #creates a balance and assigns it to the current user.
-            balance = random.randint(1,10000)
-            current_user.balance = balance
-            db.session.commit()
-            return render_template("home.html", firstName=current_user.first_name, user=current_user, balance=current_user.balance)
+    if current_user.balance:
+        return render_template("home.html", firstName=current_user.first_name, user=current_user, balance=current_user.balance)
+    #if the user does not have a balance
+    elif not current_user.balance:
+        #references auth.py's function genbal
+        balance = genbal
+        current_user.balance = balance
+        db.session.commit()
+        return render_template("home.html", firstName=current_user.first_name, user=current_user, balance=current_user.balance)
 
 @views.route("/withdrawal", methods=['GET', 'POST'])
 @login_required
 def withdrawal():
     if request.method == 'POST':
-        bal = current_user.balance
-        amount = request.form.get('amount')
-        if int(amount) <= bal:
-            current_user.balance = bal - int(amount)
-            flash(f"Withdrew {str(amount)} from your balance of {str(bal)}!", category="success")
+        amount = int(request.form.get("amount"))
+        currentbal = int(current_user.balance)
+        if amount >= 500 or currentbal - amount <= 1000:
+            return redirect(url_for("auth.passwordRequired", amount=amount))
+        else:
+            newbal = currentbal - amount
+            current_user.balance = newbal
+            db.session.commit()
+            flash("Balance successfully updated to " + str(newbal) + "! Redirecting to home...")
             return redirect(url_for("views.home"))
-
+    #get method response
     else:
-        return render_template("withdrawal.html")
+        return render_template("withdrawal.html", bal=current_user.balance, name=current_user.first_name, auth=current_user.is_authenticated, user=current_user)
+
+@views.route("/deposit", methods=['GET', 'POST'])
+@login_required
+def deposit():
+    if request.method == 'POST':
+        amount = request.form.get('amount')
+        bal = current_user.balance
+        newbal = int(bal) + int(amount)
+        current_user.balance = newbal
+        db.session.commit()
+        flash(f"You have successfully added ${str(amount)} to your balance", category="success")
+        return redirect(url_for('views.home'))
+    else:
+        return render_template("deposit.html", bal=current_user.balance, name=current_user.first_name, user=current_user)
